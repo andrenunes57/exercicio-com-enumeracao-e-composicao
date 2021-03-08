2113d55c10ee9a83d15780fb5ddb7e7e89428733
André Nunes de Lima <andre.nunes@live.com>
2021-03-08 01:45:32 -0300

    Projeto envolvendo enumeração e composição finalizado

---
+++ Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Program204.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Program204.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior  = 3
+    }
+}
